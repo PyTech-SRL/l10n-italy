34a461c5e437274c1156ddcf70e78de842aeac15
Alessandro Uffreduzzi <alessandro.uffreduzzi@pytech.it>
2022-12-28 11:30:11 +0100

    l10n_it_fatturapa_pec

--- l10n_it_fatturapa_pec/migrations/14.0.1.0.4/pre-migration.py
+++ l10n_it_fatturapa_pec/migrations/14.0.1.0.4/pre-migration.py
@@ -1,10 +1,15 @@
-from openupgradelib.openupgrade import rename_xmlids
+from openupgradelib import openupgrade
 
 
-def migrate(cr, version):
-
-    rename_xmlids(
-        cr,
+@openupgrade.migrate()
+def migrate(env, version):
+    openupgrade.delete_records_safely_by_xml_id(
+        env,
+        # Recreate the group entirely. Was not migrated correctly in the past.
+        ["l10n_it_fatturapa_out.group_force_e_inv_export_state"],
+    )
+    openupgrade.rename_xmlids(
+        env.cr,
         [
             (
                 "l10n_it_sdi_channel.sdi_pec_first_address",
