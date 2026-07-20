b2c0cc1bad65235413e7156d9febec83007411e3
Simone Rubino <simone.rubino@pytech.it>
2026-07-20 17:40:31 +0200

    WIP

--- l10n_it_delivery_note/models/sale_order.py
+++ l10n_it_delivery_note/models/sale_order.py
@@ -106,11 +106,19 @@ class SaleOrder(models.Model):
 
     def _get_invoiceable_lines(self, final=False):
         order_lines = super()._get_invoiceable_lines(final=final)
+        invoicing_delivery_notes = self.env.context.get(
+            "invoicing_delivery_notes",
+            self.env["stock.delivery.note"].browse(),
+        )
         new_order_lines = self.env["sale.order.line"].browse()
         for order_line in order_lines:
             invoiceable_dn_lines = order_line.delivery_note_line_ids.filtered(
                 lambda dn_line: dn_line.is_invoiceable
             )
+            if invoicing_delivery_notes:
+                invoiceable_dn_lines = invoiceable_dn_lines.filtered(
+                    lambda dn_line: dn_line.delivery_note_id in invoicing_delivery_notes
+                )
             if len(invoiceable_dn_lines) > 1:
                 # Add a new order line for each linked delivery note line.
                 # Every new corresponding invoice line
@@ -196,6 +204,14 @@ class SaleOrderLine(models.Model):
             self.delivery_note_line_ids.filtered(lambda dn_line: dn_line.is_invoiceable)
             - invoiced_dn_lines
         )
+        invoicing_delivery_notes = self.env.context.get(
+            "invoicing_delivery_notes",
+            self.env["stock.delivery.note"].browse(),
+        )
+        if invoicing_delivery_notes:
+            invoiceable_dn_lines = invoiceable_dn_lines.filtered(
+                lambda dn_line: dn_line.delivery_note_id in invoicing_delivery_notes
+            )
 
         if invoiceable_dn_lines:
             invoiced_dn_line = fields.first(invoiceable_dn_lines)
